a3bc46b05648386e7ab9b5104eebddcd5672fe32
Daven Quinn <dev@davenquinn.com>
2022-07-18 04:10:17 -0500

    Major update to environment handling

---
+++ env/paper-config.zsh
@@ -0,0 +1,9 @@
+export FIGURE_COLLECT_DIR="collected-figures"
+
+# Legacy files need to have allexport set
+set -o allexport
+source "$PAPER_DIR/paper-defs.zsh"
+set +o allexport
+
+# move towards modern environment variables
+export PAPER_NAME="$name"
