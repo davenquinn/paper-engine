be366d742a8b7f11b05e669e105bda00a1f7af5e
Daven Quinn <dev@davenquinn.com>
2022-07-19 04:04:07 -0500

    Updated environment

--- env/paper-config.zsh
+++ env/paper-config.zsh
@@ -3,6 +3,7 @@ export FIGURE_COLLECT_DIR="collected-figures"
 # Legacy files need to have allexport set
 set -o allexport
 source "$PAPER_DIR/paper-defs.zsh"
+source "$PAPER_DIR/.env"
 set +o allexport
 
 # move towards modern environment variables
